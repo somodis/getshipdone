6f4e06028e3d5fd1d4a335aa541ed5d692cf57e7
somodis <its.somodi@gmail.com>
2022-09-25 13:23:56 +0200

    feat(log): SQL logger added using TypeOrmLogger

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/validate';
 import LogsMiddleware from './common/middleware/logs.middleware';
+import DatabaseLogger from './common/databaseLogger';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import LogsMiddleware from './common/middleware/logs.middleware';
       type: 'sqlite',
       database: path.resolve(__dirname, '../data/db.sqlite'),
       entities: ['dist/database/entity/*.entity{.ts,.js}'],
+      logger: new DatabaseLogger(),
       logging: process.env.NODE_ENV !== 'production',
       synchronize: process.env.NODE_ENV !== 'production',
     }),
